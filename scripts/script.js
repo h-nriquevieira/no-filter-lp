4808eef1b2aaff4e692acd6180119e8b254dfb24
h-nriquevieira <h.nriquevieira@gmail.com>
2022-07-03 16:17:22 -0300

    Update caroussel functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 // Before & After functionality
 
-const $img1 = document.querySelector('.js-img-1');
-const $img2 = document.querySelector('.js-img-2');
 const $beforeAfterTitle = document.querySelector('.js-before-after-title');
 const $beforeAfterDescription = document.querySelector('.js-before-after-description');
 const $arrowLeft = document.querySelector('.js-arrow-left');
@@ -11,10 +9,14 @@ let counter = 0;
 
 const content = [
     {
+        before: document.querySelector('.js-img-before-1'),
+        after: document.querySelector('.js-img-after-1'),
         title: 'Lympathic Drainage',
         description: 'This technique combines exclusives manouvers that deliver immediate treatment to results! The body benefits from a reduction in swelling, gains a better defined contour, accelerates its metabolism and heals faster. It also provides stress reduction and increase of a well-being sensation.'
     },
     {
+        before: document.querySelector('.js-img-before-2'),
+        after: document.querySelector('.js-img-after-2'),
         title: 'Custom Peel',
         description: 'Our custom peels will work help reducing fine lines and wrinkles, lightening pigmentation, healing acne, reducing scarring, even skin texture. Peels is one of our expertise so rest assured we know which blend is best for you.'
     }
@@ -26,8 +28,10 @@ function nextSlide() {
     } else {
         counter++;
     }
-    $img1.src = `./img/before-${counter + 1}.png`;
-    $img2.src = `./img/after-${counter + 1}.png`;
+    content[counter-1].before.classList.add('hidden');
+    content[counter-1].after.classList.add('hidden');
+    content[counter].before.classList.remove('hidden');
+    content[counter].after.classList.remove('hidden');
     $beforeAfterTitle.textContent = content[counter].title;
     $beforeAfterDescription.textContent = content[counter].description;
 }
@@ -38,8 +42,10 @@ function previousSlide() {
     } else {
         counter--;
     }
-    $img1.src = `./img/before-${counter + 1}.png`;
-    $img2.src = `./img/after-${counter + 1}.png`;
+    content[counter+1].before.classList.add('hidden');
+    content[counter+1].after.classList.add('hidden');
+    content[counter].before.classList.remove('hidden');
+    content[counter].after.classList.remove('hidden');
     $beforeAfterTitle.textContent = content[counter].title;
     $beforeAfterDescription.textContent = content[counter].description;
 }
